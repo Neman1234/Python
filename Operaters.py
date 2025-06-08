6aed076d8e8ad9d8df60cc59f03ea15730371139
Neman1234 <Neelmansata2@gmail.com>
2025-06-08 18:30:50 -0400

    Add files via upload

---
+++ Operaters.py
@@ -0,0 +1,9 @@
+a = 3
+b = 4
+## Arithmetic Operators
+
+print("This is the addition of variable a and b :",a+b)
+print("The value of 3-4 is ", a-b)
+print("The value of 3*4 is ", a*b)
+print("The value of 3/4 is ", a/b)
+print("The value of 3 power 4 is ", a**b)
---
+++ Variable-Start.py
@@ -0,0 +1,12 @@
+e1="Hello World"
+e2=500+250
+e3=420.5
+e4=e3-e2
+print(e1)
+print(e2)
+print(e3)
+print(e4)
+print(type(e1))
+print(type(e2))
+print(type(e3))
+print(type(e4))
