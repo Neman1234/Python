4d9c544e04eb2b6b823afdb2e6e07563e3f85bc2
Neman1234 <Neelmansata2@gmail.com>
2025-06-11 16:59:29 -0400

    Add files via upload

--- Operaters.py
+++ Operaters.py
@@ -1,9 +1,34 @@
-a = 3
-b = 4
-## Arithmetic Operators
+#a = 3
+#b = 4
+# Arithmetic Operators
 
-print("This is the addition of variable a and b :",a+b)
-print("The value of 3-4 is ", a-b)
-print("The value of 3*4 is ", a*b)
-print("The value of 3/4 is ", a/b)
-print("The value of 3 power 4 is ", a**b)
+#print("This is the addition of variable a and b :",a+b)
+#print("The value of 3-4 is ", a-b)
+#print("The value of 3*4 is ", a*b)
+#print("The value of 3/4 is ", a/b)
+#print("The value of 3 power 4 is ", a**b)
+
+a=34
+a=a-12 #normal method
+a-=12 #shortand method
+a*=12
+a**=12
+a/=12
+
+a=10.00000
+b=10
+#comparison Operators
+c=(a<b)
+d=(a>b)
+e=(a>=b)
+f=(a==b)
+g=(a!=b)
+
+#Print Outputs
+print(round(a))
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
