e41b01519476069a0cc7b66fa6feb2650bbaff23
Neman1234 <Neelmansata2@gmail.com>
2025-06-13 17:58:35 -0400

    Add files via upload

---
+++ Type-Casting.py
@@ -0,0 +1,27 @@
+#Operators Practice
+a=10
+b=20
+c=20.9000
+print("This is 10+20 =",a+b)
+print(a**b)
+print(a*b)
+print(a/b)
+print(a-b)
+print(a==b)
+print(a<b)
+print(a>b)
+print(b==a)
+print(b>a)
+print(b<a)
+print(a!=b)
+
+#type casting
+a=str(a)
+b=float(b)
+c=int(c)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
