d26414138240fb9870d27335eddbecdf5ecaf158
Neman1234 <Neelmansata2@gmail.com>
2025-06-23 13:58:49 -0400

    Add files via upload

---
+++ String-function.py
@@ -0,0 +1,3 @@
+#create variables
+Story = "In the quiet, mist-wrapped town of Elmridge,n where an old clocktower stood frozen at 3:17 and ignored by wary townsfolk who feared its whispers,\n twelve-year-old Mira—skeptical of ghost tales—followed the tower’s eerie midnight call, crept across the foggy square with flashlight in hand,\n ascended the spiral stairs as whispers grew louder with every step, discovered her name carved into the wood beside a keyhole, reached instinctively into her pocket to find a key she never knew she had, and with a trembling hand turned it, awakening the slumbering gears, ringing the bells, stirring the fog, and unlocking not just the hands of time, but the lost memories and buried truths of a town held still for over a hundred years."
+print(Story)
---
+++ Strings.py
@@ -0,0 +1,9 @@
+#Create variables
+Sentance="Hi My Name is Neel"
+
+#Slice sentence
+print(Sentance[0:3])
+print(Sentance[3:10])
+print(Sentance[:10])
+print(Sentance[3:])
+print(Sentance[-5:-1])
