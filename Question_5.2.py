d3b728b5e5e56de032de41ccdd0c6c4037ac08be
Neman1234 <Neelmansata2@gmail.com>
2025-07-02 10:33:17 -0400

    Add files via upload

---
+++ Question_5.2.py
@@ -0,0 +1,9 @@
+#take numbers from the user
+first_Number=int(input("Enter a Number "))
+second_Number=int(input("Enter another number "))
+
+#get the answer
+answer=first_Number+second_Number/2
+
+#print the answer
+print(int(answer))
---
+++ Question_6.2.py
@@ -0,0 +1,8 @@
+#take number from user
+Number=int(input("Enter a Number "))
+
+#get answer
+answer=Number*Number
+
+#print answer
+print(answer)
