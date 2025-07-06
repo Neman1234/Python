7a47f10bba30362a00368e2e0c75db77602041c1
Neman1234 <Neelmansata2@gmail.com>
2025-07-06 14:58:09 -0400

    Add files via upload

---
+++ List_Slicing.py
@@ -0,0 +1,18 @@
+#Create list
+friends=["Harry","Tom","Rohan","Sam","Divya"]
+numbers=[1,2,3,4,5]
+
+#slice the list
+print(friends[0:3])        #This will print the element from 0 to 3
+print(friends[2:5])        #This will print the element from 2 to 5
+print(friends[1:])
+print(friends[:3])
+print(friends[-3:])
+
+#Add all the items in the list
+answer= numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]
+
+#print answer
+print(answer)
+
+#print one item in the list
