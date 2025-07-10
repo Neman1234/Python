44734644ad28cee7d5e38ca6f9e1aa72df8e30fd
Neman1234 <Neelmansata2@gmail.com>
2025-07-10 10:53:59 -0400

    Add files via upload

--- question_8.1.py
+++ question_8.1.py
@@ -1,5 +1,14 @@
-#create list
-nums=[5,10,15,20,25]
+#take input from user
+fruit_1=str(input("Enter a Fruit "))
+fruit_2=str(input("Enter another Fruit "))
+fruit_3=str(input("Enter another Fruit "))
+fruit_4=str(input("Enter another Fruit "))
+fruit_5=str(input("Enter another Fruit "))
+fruit_6=str(input("Enter another Fruit "))
+fruit_7=str(input("Enter another Fruit "))
 
-#slice the list
-print(nums[0:3])
+#create list
+Your_Favorite_Fruits=[fruit_1,fruit_2,fruit_3,fruit_4,fruit_5,fruit_6,fruit_7]
+
+#print list
+print(Your_Favorite_Fruits)
