673a26da9d5c70ff9ddddae01523e8a522eceebb
Neman1234 <Neelmansata2@gmail.com>
2025-07-10 10:42:42 -0400

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,24 @@
+#Create Tuple
+Number_order=(1,2,3,4,5,6,7,8,9,10)
+TWO_D=("paper",)
+Empty_Tuple=()
+
+#print tuple
+print(Number_order)
+
+#print type of tuple
+print(type(Number_order))
+
+#print one item from tuple]
+print(Number_order[9])
+
+#try to update tuple
+#Number_order[1]=0
+
+#you can't update or change or delete or insert values from a tuple
+
+#put one item in a tuple
+print(TWO_D[0])
+
+#print empty tuple
+print(Empty_Tuple)
