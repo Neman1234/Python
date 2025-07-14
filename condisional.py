95e6d6b1b14decfe0614c1272467116fac2e17d7
Neman1234 <Neelmansata2@gmail.com>
2025-07-14 15:58:29 -0400

    Add files via upload

--- condisional.py
+++ condisional.py
@@ -1,11 +1,24 @@
-a=33
-b=33
-if a>b:
-  print("true")
-else:
-  print("false")
+#class 15 code
+#a=33
+#b=33
+#if a>b:
+#  print("true")
+#else:
+#  print("false")
 
-if a<b:
-  print("true2")
-elif b>a:
-  print("false2")
+#if a<b:
+#  print("true2")
+#elif b>a:
+#  print("false2")
+
+#class 16 code
+a1=200
+b1=33
+if b1>a1:
+  print("b is greater then a")
+elif a1==b1:
+  print("a and b are equal")
+elif b1<a1:
+  print("b is less then a")
+else:
+  print("a is greater then b")
---
+++ condisional_function.py
@@ -0,0 +1,17 @@
+#Take input from user
+Age=int(input("Type a Number "))
+
+#check if is adult or kid
+if Age>17:
+  print("adult")
+else:
+  print("kid")
+
+#take input from user
+a=int(input("Type in a Number "))
+
+#check if number is divisible by 2
+answer=a%2
+
+#print answer
+print(answer)
