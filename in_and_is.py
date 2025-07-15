a75085b53e0eef542b6586242a2acfa1396d945d
Neman1234 <Neelmansata2@gmail.com>
2025-07-15 11:21:46 -0400

    Add files via upload

---
+++ in_and_is.py
@@ -0,0 +1,15 @@
+#create variables and lists and tuples
+a=446
+b=[3,6,9,8,"Neel"]
+c=(1,4,7,10,"Neel")
+
+#use is
+if(446 is a ):
+  print("true")
+
+#use in
+if('''Neel''' in b ):
+  print("true2")
+
+if('''Neel''' in c ):
+  print("true3")
