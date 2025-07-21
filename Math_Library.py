1b73e1175836eb125eb6903a951d5dd0c6d17364
Neman1234 <Neelmansata2@gmail.com>
2025-07-21 15:47:42 -0400

    Add files via upload

--- Math_Library.py
+++ Math_Library.py
@@ -1,4 +1,13 @@
+#class 19 code
 import math
 print(math.pow(2,2))
 print(math.floor(4.9))
 print(math.ceil(4.1))
+print(math.pi)
+
+#class 20 code
+num=0.18
+print(math.sqrt(num))
+print(math.factorial(5))
+print(math.fabs(-5.5))
+print(math.gcd(209,508))
