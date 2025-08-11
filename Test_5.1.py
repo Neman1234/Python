2789615139f936b6e78fcc864a61ba2f2a26e9e1
Neman1234 <Neelmansata2@gmail.com>
2025-08-11 11:01:56 -0400

    Add files via upload

---
+++ Test_5.1.py
@@ -0,0 +1,20 @@
+Neel = {
+ "Age": 9,
+ "City": "Ashburn",
+ "Language": "English And Hindi"
+}
+
+print(Neel["Age"])
+
+Neel["City"] = "San hosie"
+
+print(Neel["City"])
+
+print(Neel.keys())
+print(Neel.values())
+print(Neel.items())
+
+
+Neel["Favorte Coler"] = "red"
+
+print(Neel)
