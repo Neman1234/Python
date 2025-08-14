8415ffe2d8a482c5c58adaa706d01e1882b0ae3f
Neman1234 <Neelmansata2@gmail.com>
2025-08-14 11:00:06 -0400

    Add files via upload

---
+++ Set_Methods.py
@@ -0,0 +1,33 @@
+#Create set
+Temp = set()
+
+#add to set
+Temp.add("⭐")
+Temp.add(5500)
+Temp.add(True)
+Temp.add("Neel")
+Temp.add(6700.512347896)
+
+#Try to add list
+#Temp.add([100000,"Neel","🚨"])
+
+#Try to add tuple
+Temp.add((100000,"Neel","🪙"))
+
+#print set
+print(Temp)
+
+#check length of set
+print(len(Temp))
+
+#use . remove
+Temp.remove(True)
+
+#print temp
+print(Temp)
+
+# use . pop
+Temp.pop()
+
+#print temp
+print(Temp)
---
+++ Sets.py
@@ -0,0 +1,9 @@
+#Create sets
+i = {1,1,1,1,1,1,1,1,1,1}
+A = {"Neel",1916,"Shark attack"}
+Temp = set()
+
+#Print Sets
+print(i)
+print(A)
+print(Temp)
