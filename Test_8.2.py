9258af36aee0ca69ecafe39ae3c2c83b9c6e04a5
Neman1234 <Neelmansata2@gmail.com>
2025-09-12 17:58:07 -0400

    Add files via upload

---
+++ Test_8.2.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number with no decimals "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i   
+
+print(factorial )
---
+++ Test_9.1.py
@@ -0,0 +1,4 @@
+st1 = {1,2}
+st2 = {"Neel",2}
+intersection_set = st1.intersection(st2)
+print(intersection_set)
