8353f56c973f842018af9a99775a86798770a3ee
Neman1234 <Neelmansata2@gmail.com>
2026-01-07 16:30:44 -0500

    Add files via upload

---
+++ Review_Class_1.py
@@ -0,0 +1,11 @@
+# Create Variables of different data types
+a = 45
+b = 678.889
+c = "Hello world"
+d = True
+
+#Print the type of each variable
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'float'>
+print(type(c))  # Output: <class 'str'>
+print(type(d))  # Output: <class 'bool'>
