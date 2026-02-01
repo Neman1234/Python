ecd957e5133e83da8af9ac54e59c7cea6160cfdc
Neman1234 <Neelmansata2@gmail.com>
2026-02-01 21:34:47 +0530

    Add files via upload

---
+++ Review_Class_3.py
@@ -0,0 +1,10 @@
+DataTypes = [123,"Hello",12.0,True]
+IntegerList = [13,79488,4534]
+
+
+print(DataTypes[1])
+print(len(DataTypes))
+print(max(IntegerList))
+print(min(IntegerList))
+DataTypes[1]="hi"
+print(DataTypes)
