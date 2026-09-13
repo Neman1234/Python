2097dedd6ddf676f08bc1aaa0bb995ffcd4162d1
Neman1234 <Neelmansata2@gmail.com>
2026-09-13 09:51:28 -0400

    Add files via upload

---
+++ arrays.py.py
@@ -0,0 +1,22 @@
+arr = [5, 2, 8, 1, 9, 3]
+add = 0
+for x in arr:
+    add += x
+
+print(add)
+
+
+
+arr = [5, 2, 8, 1, 9, 3]
+ 
+smallest = arr[0]
+ 
+for x in arr:
+    if x < smallest:
+        smallest = x
+ 
+print(smallest)
+
+
+
+print(arr(min))
