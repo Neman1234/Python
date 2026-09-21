d626c8bcb35c6d295679f09a52083602787dfe9d
Neman1234 <Neelmansata2@gmail.com>
2026-09-21 09:57:19 -0400

    Add files via upload

---
+++ Add_Five_Numbers_Total_Version.py
@@ -0,0 +1,12 @@
+number_1 = float(input("Enter a number "))
+number_2 = float(input("Enter a number "))
+number_3 = float(input("Enter a number "))
+number_4 = float(input("Enter a number "))
+number_5 = float(input("Enter a number "))
+
+def add_numbers():
+    Total_Numbers = number_1 + number_2 +number_3 + number_4 + number_5
+    print(Total_Numbers)
+
+
+add_numbers()
