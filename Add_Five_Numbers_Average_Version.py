79bfbe250533b95fb61dff953d05749ee8d8e1dc
Neman1234 <Neelmansata2@gmail.com>
2026-09-21 10:10:15 -0400

    Add files via upload

---
+++ Add_Five_Numbers_Average_Version.py
@@ -0,0 +1,13 @@
+number_1 = float(input("Enter a number "))
+number_2 = float(input("Enter a number "))
+number_3 = float(input("Enter a number "))
+number_4 = float(input("Enter a number "))
+number_5 = float(input("Enter a number "))
+
+def add_numbers_and_get_average():
+    Total_Numbers = number_1 + number_2 +number_3 + number_4 + number_5
+    Average = Total_Numbers/5
+    print(Average)
+
+
+add_numbers_and_get_average()
